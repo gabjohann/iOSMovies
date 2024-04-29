a960bbb656ffe23c03c4c8babc209738c97f7699
Kretzmann01 <gabrielkretzmanndasilva@hotmail.com>
2024-04-29 10:20:55 -0300

    feat: modelagem de dados

---
+++ iOSMovies/Models/Serie.swift
@@ -0,0 +1,33 @@
+//
+//  Serie.swift
+//  iOSMovies
+//
+//  Created by ios-manha-03 on 29/04/24.
+//
+
+import Foundation
+
+struct Serie: Decodable {
+    let id: String
+    let title: String
+    let released: String?
+    let language: String?
+    let country: String?
+    let genre: String?
+    let plot: String?
+    let posterURL: String?
+    
+    var isFavorite: Bool = false
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "imdbID"
+        case title = "Title"
+        case released = "Released"
+        case language = "Language"
+        case country = "Country"
+        case genre = "Genre"
+        case plot = "Plot"
+        case posterURL = "Poster"
+    }
+
+}
